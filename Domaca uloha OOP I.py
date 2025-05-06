6e37f39bcb4ac5419a6f5d125b86ca887d5337bd
MarekPisarcik <pisar385@gmail.com>
2025-05-06 18:02:01 +0200

    1.3
    1.3 Použitie property a setter metódy

--- Domaca uloha OOP I.py
+++ Domaca uloha OOP I.py
@@ -11,11 +11,23 @@ class Package:
 # 1.2 Metody v triede
     def __str__(self):
         return f"Balik pre: {self.sender}, Od: {self.recipient}, Vaha: {self.weight}, Stav: {self.status}"
-
+# 1.3 Getter
+    @property
+    def delivered(self):
+        return self.status == "Doruceny"
+    # Setter
+    @delivered.setter
+    def delivered(self, nova_hodnota):
+        if nova_hodnota:
+            self.status = "Doruceny"
+        else:
+            self.status = "Na ceste"
 balik1 = Package("School", "Marek", "0.5", "Na ceste")
 balik2 = Package("Work", "Radovit", "2", "Doruceny")
 
 print(balik1.__str__())
 print(balik2.__str__())
 
-
+balik1.delivered = False
+print(balik1.delivered)
+print(balik1.status)
